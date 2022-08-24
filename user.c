0c87eb5cae5917bb07d9df2b230c460b7cdc01b9
nboufford <ncboufford@gmail.com>
2022-08-24 16:28:00 +0000

    add license to each file

--- user.c
+++ user.c
@@ -1,10 +1,22 @@
-/* SPDX-License-Identifier: GPL-2.0 */
-
 /*
- * Author: Nichole Boufford
+ * 
+ * Author: Nichole Boufford <ncbouf@cs.ubc.ca>
+ *
+ * Copyright (C) 2022 University of British Columbia
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *  https://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ *
  */
-
-// from: https://git.kernel.org/pub/scm/linux/kernel/git/stable/linux.git/tree/tools/testing/selftests/bpf/prog_tests/test_lsm.c
 
 #include <sys/mman.h>
 #include <sys/wait.h>
