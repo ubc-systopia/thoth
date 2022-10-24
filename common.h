8810f6ea65482d7906adeea38eec16477b3cd2bd
nboufford <ncboufford@gmail.com>
2022-10-24 13:40:56 +0000

    implement cli write to sever

--- common.h
+++ common.h
@@ -24,4 +24,32 @@
 #define SOCK_BUF_MAX 256
 #define CLI_PORT 5001
 
+#define HOSTNAME_MAX 10
+
+#define NUM_ARGS 5
+#define ARG_LEN 50
+#define MSG_LEN 150
+
+#define INODE_MAX_ENTRY 256
+
+enum cli_op {
+  ADD_DIR = 0,
+  RM_DIR = 1,
+};
+
+enum cli_err {
+  ERR_OK = 0,
+};
+
+struct op_msg {
+  enum cli_op op;
+  char arg[NUM_ARGS][ARG_LEN];
+};
+
+struct err_msg {
+  enum cli_err err;
+  char msg[MSG_LEN];
+};
+
+
 #endif
--- thoth.c
+++ thoth.c
@@ -22,7 +22,11 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
+#include <netinet/in.h>
+#include <netdb.h>
+#include <syslog.h>
 
+#include "common.h"
 
 #define ARG_TRACK_DIR     "--track-dir"
 #define ARG_REMOVE_DIR    "--remove-dir"
@@ -30,11 +34,62 @@
 #define MATCH_ARGS(str1, str2) if (strcmp(str1, str2) == 0)
 #define CHECK_ATTR(argc, min) if (argc < min) { exit(-1); }
 
+int write_to_server(struct op_msg msg) {
+  int sockfd;
+  struct sockaddr_in server_addr;
+  char hostname[HOSTNAME_MAX];
+  
+  gethostname(hostname, HOSTNAME_MAX);
+
+  sockfd = socket(AF_INET, SOCK_STREAM, 0);
+  if (sockfd < 0) {
+    perror("Error opening socket");
+    return -1;
+  }
+
+  bzero((char *) &server_addr, sizeof(server_addr));
+  server_addr.sin_family = AF_INET;
+  struct hostent* hptr = gethostbyname(hostname);
+  bcopy((char *)hptr->h_addr, (char *)&server_addr.sin_addr.s_addr, hptr->h_length);
+  server_addr.sin_port = htons(CLI_PORT);
+
+  syslog(LOG_INFO, "thoth: connecting to host: %s, port: %d\r\n", hostname, server_addr.sin_port);
+
+  if (connect(sockfd, (struct sockaddr*)&server_addr, sizeof(server_addr)) < 0)
+    perror("error connecting to server");
+  
+  write(sockfd, &msg, sizeof(msg)); 
+  syslog(LOG_INFO, "thoth: writing message to socket: %s\r\n", msg.arg[0]);
+
+  struct err_msg response;
+  int n = read(sockfd, &response, sizeof(response));
+  if (n < 0)
+    perror("error: no response from server");
+  
+  // print server response
+  printf("%s\r\n", response.msg);
+  
+  close(sockfd);
+  return 0;
+}
+
 int add_dir(char* arg) {
+  // format and write to server
+  struct op_msg msg = {
+    .op = ADD_DIR,
+  };
+  snprintf(msg.arg[0], ARG_LEN, "%s", arg);
+  write_to_server(msg);
   return 0;
 }
 
 int remove_dir(char* arg) {
+  // format and write to server
+  struct op_msg msg = {
+    .op = RM_DIR,
+  };
+  snprintf(msg.arg[0], ARG_LEN, "%s", arg);
+  write_to_server(msg);
   return 0;
 }
 
@@ -43,7 +98,6 @@ int main(int argc, char *argv[])
   CHECK_ATTR(argc, 2);
 
   MATCH_ARGS(argv[1], ARG_TRACK_DIR) {
-    printf("hello\r\n");
     CHECK_ATTR(argc, 3);
     add_dir(argv[2]);
     return 0;
@@ -54,6 +108,8 @@ int main(int argc, char *argv[])
     remove_dir(argv[2]); 
     return 0;
   }
+
+  printf("Command not found!\r\n");
 
   return 0; 
 }
