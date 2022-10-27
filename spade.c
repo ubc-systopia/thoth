f29045801697c29a453a364011e3f8fe09ec2ee0
nboufford <ncboufford@gmail.com>
2022-10-27 21:17:30 +0000

    uncrustify

--- spade.c
+++ spade.c
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  * Author: Nichole Boufford <ncbouf@cs.ubc.ca>
  *
  * Copyright (C) 2022 University of British Columbia
@@ -24,173 +24,187 @@
 
 #include "record.h"
 
-#define MAX_BUFFER_LEN 1024 
+#define MAX_BUFFER_LEN 1024
 
 int edge_id = 0;
 
-void add_name(char* buffer, char* name) {
-  strncat(buffer, "\"", MAX_BUFFER_LEN);
-  strncat(buffer, name, MAX_BUFFER_LEN);
-  strncat(buffer, "\":", MAX_BUFFER_LEN);
-} 
-
-void spade_write_node_proc(int fd, struct entry_t *entry) {
-  char buf[MAX_BUFFER_LEN];
-  buf[0]='\0';
-  
-  char pid[32];
-  sprintf(pid, "%u", entry->pid);
-  char inode[32];
-  sprintf(inode, "%u", entry->inode_inum);
-
-  // start json
-  strncat(buf, "{", MAX_BUFFER_LEN);
- 
-  // type 
-  strncat(buf, "\"type\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"Activity\",", MAX_BUFFER_LEN);
-
-  // id
-  strncat(buf, "\"id\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN);
-  strncat(buf, pid, MAX_BUFFER_LEN);
-  strncat(buf, "\",", MAX_BUFFER_LEN);
-  
-  // annotations
-  strncat(buf, "\"annotations\":{", MAX_BUFFER_LEN);  
-
-  strncat(buf, "\"pid\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN);
-  strncat(buf, pid, MAX_BUFFER_LEN);
-  strncat(buf, "\",", MAX_BUFFER_LEN);
- 
-  // end json
-  strncat(buf, "}}\n", MAX_BUFFER_LEN);
-  write(fd, buf, strnlen(buf, MAX_BUFFER_LEN));
+void add_name(char *buffer, char *name)
+{
+	strncat(buffer, "\"", MAX_BUFFER_LEN);
+	strncat(buffer, name, MAX_BUFFER_LEN);
+	strncat(buffer, "\":", MAX_BUFFER_LEN);
 }
 
-void spade_write_node_file(int fd, struct entry_t *entry, char* buffer) {
-  char buf[MAX_BUFFER_LEN];
-  buf[0] = '\0';
-  
-  char inode[32];
-  sprintf(inode, "%u", entry->inode_inum);
-  char uid[32];
-  sprintf(uid, "%u", entry->inode_uid);
-  // start json
-  strncat(buf, "{", MAX_BUFFER_LEN);
+void spade_write_node_proc(int fd, struct entry_t *entry)
+{
+	char buf[MAX_BUFFER_LEN];
 
-  // type
-  strncat(buf, "\"type\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"Entity\",", MAX_BUFFER_LEN);
+	buf[0] = '\0';
 
-  // id
-  strncat(buf, "\"id\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN);
-  strncat(buf, inode, MAX_BUFFER_LEN);
-  strncat(buf, "\",", MAX_BUFFER_LEN);
-  
-  // annotations
-  strncat(buf, "\"annotations\":{", MAX_BUFFER_LEN);  
+	char pid[32];
 
-  strncat(buf, "\"inode_inum\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN);
-  strncat(buf, inode, MAX_BUFFER_LEN);
-  strncat(buf, "\",", MAX_BUFFER_LEN);
- 
-  strncat(buf, "\"uid\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN);
-  strncat(buf, uid, MAX_BUFFER_LEN);
-  strncat(buf, "\",", MAX_BUFFER_LEN);
-  
-  strncat(buf, "\"path\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN);
-  strncat(buf, buffer, MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN); 
+	sprintf(pid, "%u", entry->pid);
+	char inode[32];
 
-  // end json
-  strncat(buf, "}}\n", MAX_BUFFER_LEN);
+	sprintf(inode, "%u", entry->inode_inum);
 
-  write(fd, buf, strnlen(buf, MAX_BUFFER_LEN));
+	// start json
+	strncat(buf, "{", MAX_BUFFER_LEN);
+
+	// type
+	strncat(buf, "\"type\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"Activity\",", MAX_BUFFER_LEN);
+
+	// id
+	strncat(buf, "\"id\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+	strncat(buf, pid, MAX_BUFFER_LEN);
+	strncat(buf, "\",", MAX_BUFFER_LEN);
+
+	// annotations
+	strncat(buf, "\"annotations\":{", MAX_BUFFER_LEN);
+
+	strncat(buf, "\"pid\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+	strncat(buf, pid, MAX_BUFFER_LEN);
+	strncat(buf, "\",", MAX_BUFFER_LEN);
+
+	// end json
+	strncat(buf, "}}\n", MAX_BUFFER_LEN);
+	write(fd, buf, strnlen(buf, MAX_BUFFER_LEN));
 }
 
-void spade_write_edge(int fd, struct entry_t *entry) {
-  char buf[MAX_BUFFER_LEN];
-  buf[0] = '\0';
-  
-  char id[32];
-  sprintf(id, "%u", edge_id++);
-  char pid[32];
-  sprintf(pid, "%u", entry->pid);
-  char inode[32];
-  sprintf(inode, "%u", entry->inode_inum);
-  char utime[32];
-  sprintf(utime, "%u", entry->utime);
+void spade_write_node_file(int fd, struct entry_t *entry, char *buffer)
+{
+	char buf[MAX_BUFFER_LEN];
 
-  char operation[32];
-  if (entry->op == READ) {
-    sprintf(operation, "%s", "read");
-  } else if (entry->op == WRITE) {
-    sprintf(operation, "%s", "write");
-  } else if (entry->op == EXEC) {
-    sprintf(operation, "%s", "execute");
-  }
+	buf[0] = '\0';
 
-  // start json
-  strncat(buf, "{", MAX_BUFFER_LEN);
+	char inode[32];
 
-  // type
-  strncat(buf, "\"type\":", MAX_BUFFER_LEN);
-  if (entry->op == READ) {
-    strncat(buf, "\"Used\",", MAX_BUFFER_LEN);
-  } else if (entry->op == WRITE) {
-    strncat(buf, "\"WasGeneratedBy\",", MAX_BUFFER_LEN);
-  } else if (entry->op == EXEC) {
-    strncat(buf, "\"Used\",", MAX_BUFFER_LEN);
-  }
+	sprintf(inode, "%u", entry->inode_inum);
+	char uid[32];
 
-  if (entry->op == READ || entry->op == EXEC) {
-    // to
-    strncat(buf, "\"to\":", MAX_BUFFER_LEN);
-    strncat(buf, "\"", MAX_BUFFER_LEN);
-    strncat(buf, inode, MAX_BUFFER_LEN);
-    strncat(buf, "\",", MAX_BUFFER_LEN);
- 
-    // from
-    strncat(buf, "\"from\":", MAX_BUFFER_LEN);
-    strncat(buf, "\"", MAX_BUFFER_LEN);
-    strncat(buf, pid, MAX_BUFFER_LEN);
-    strncat(buf, "\",", MAX_BUFFER_LEN);
-  } else if (entry->op == WRITE) {
-     // to
-    strncat(buf, "\"to\":", MAX_BUFFER_LEN);
-    strncat(buf, "\"", MAX_BUFFER_LEN);
-    strncat(buf, pid, MAX_BUFFER_LEN);
-    strncat(buf, "\",", MAX_BUFFER_LEN);
- 
-    // from
-    strncat(buf, "\"from\":", MAX_BUFFER_LEN);
-    strncat(buf, "\"", MAX_BUFFER_LEN);
-    strncat(buf, inode, MAX_BUFFER_LEN);
-    strncat(buf, "\",", MAX_BUFFER_LEN);
-  }
-  
-  // annotations
-  strncat(buf, "\"annotations\":{", MAX_BUFFER_LEN);  
+	sprintf(uid, "%u", entry->inode_uid);
+	// start json
+	strncat(buf, "{", MAX_BUFFER_LEN);
 
-  strncat(buf, "\"operation\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN);
-  strncat(buf, operation, MAX_BUFFER_LEN);
-  strncat(buf, "\",", MAX_BUFFER_LEN);
- 
-  strncat(buf, "\"utime\":", MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN);
-  strncat(buf, utime, MAX_BUFFER_LEN);
-  strncat(buf, "\"", MAX_BUFFER_LEN);
-  
-  // end json
-  strncat(buf, "}}\n", MAX_BUFFER_LEN);
+	// type
+	strncat(buf, "\"type\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"Entity\",", MAX_BUFFER_LEN);
 
-  write(fd, buf, strnlen(buf, MAX_BUFFER_LEN));
+	// id
+	strncat(buf, "\"id\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+	strncat(buf, inode, MAX_BUFFER_LEN);
+	strncat(buf, "\",", MAX_BUFFER_LEN);
+
+	// annotations
+	strncat(buf, "\"annotations\":{", MAX_BUFFER_LEN);
+
+	strncat(buf, "\"inode_inum\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+	strncat(buf, inode, MAX_BUFFER_LEN);
+	strncat(buf, "\",", MAX_BUFFER_LEN);
+
+	strncat(buf, "\"uid\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+	strncat(buf, uid, MAX_BUFFER_LEN);
+	strncat(buf, "\",", MAX_BUFFER_LEN);
+
+	strncat(buf, "\"path\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+	strncat(buf, buffer, MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+
+	// end json
+	strncat(buf, "}}\n", MAX_BUFFER_LEN);
+
+	write(fd, buf, strnlen(buf, MAX_BUFFER_LEN));
+}
+
+void spade_write_edge(int fd, struct entry_t *entry)
+{
+	char buf[MAX_BUFFER_LEN];
+
+	buf[0] = '\0';
+
+	char id[32];
+
+	sprintf(id, "%u", edge_id++);
+	char pid[32];
+
+	sprintf(pid, "%u", entry->pid);
+	char inode[32];
+
+	sprintf(inode, "%u", entry->inode_inum);
+	char utime[32];
+
+	sprintf(utime, "%u", entry->utime);
+
+	char operation[32];
+
+	if (entry->op == READ)
+		sprintf(operation, "%s", "read");
+	else if (entry->op == WRITE)
+		sprintf(operation, "%s", "write");
+	else if (entry->op == EXEC)
+		sprintf(operation, "%s", "execute");
+
+	// start json
+	strncat(buf, "{", MAX_BUFFER_LEN);
+
+	// type
+	strncat(buf, "\"type\":", MAX_BUFFER_LEN);
+	if (entry->op == READ)
+		strncat(buf, "\"Used\",", MAX_BUFFER_LEN);
+	else if (entry->op == WRITE)
+		strncat(buf, "\"WasGeneratedBy\",", MAX_BUFFER_LEN);
+	else if (entry->op == EXEC)
+		strncat(buf, "\"Used\",", MAX_BUFFER_LEN);
+
+	if (entry->op == READ || entry->op == EXEC) {
+		// to
+		strncat(buf, "\"to\":", MAX_BUFFER_LEN);
+		strncat(buf, "\"", MAX_BUFFER_LEN);
+		strncat(buf, inode, MAX_BUFFER_LEN);
+		strncat(buf, "\",", MAX_BUFFER_LEN);
+
+		// from
+		strncat(buf, "\"from\":", MAX_BUFFER_LEN);
+		strncat(buf, "\"", MAX_BUFFER_LEN);
+		strncat(buf, pid, MAX_BUFFER_LEN);
+		strncat(buf, "\",", MAX_BUFFER_LEN);
+	} else if (entry->op == WRITE) {
+		// to
+		strncat(buf, "\"to\":", MAX_BUFFER_LEN);
+		strncat(buf, "\"", MAX_BUFFER_LEN);
+		strncat(buf, pid, MAX_BUFFER_LEN);
+		strncat(buf, "\",", MAX_BUFFER_LEN);
+
+		// from
+		strncat(buf, "\"from\":", MAX_BUFFER_LEN);
+		strncat(buf, "\"", MAX_BUFFER_LEN);
+		strncat(buf, inode, MAX_BUFFER_LEN);
+		strncat(buf, "\",", MAX_BUFFER_LEN);
+	}
+
+	// annotations
+	strncat(buf, "\"annotations\":{", MAX_BUFFER_LEN);
+
+	strncat(buf, "\"operation\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+	strncat(buf, operation, MAX_BUFFER_LEN);
+	strncat(buf, "\",", MAX_BUFFER_LEN);
+
+	strncat(buf, "\"utime\":", MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+	strncat(buf, utime, MAX_BUFFER_LEN);
+	strncat(buf, "\"", MAX_BUFFER_LEN);
+
+	// end json
+	strncat(buf, "}}\n", MAX_BUFFER_LEN);
+
+	write(fd, buf, strnlen(buf, MAX_BUFFER_LEN));
 
 }
--- user.c
+++ user.c
@@ -1,5 +1,5 @@
 /*
- * 
+ *
  * Author: Nichole Boufford <ncbouf@cs.ubc.ca>
  *
  * Copyright (C) 2022 University of British Columbia
@@ -42,234 +42,240 @@ static struct track *skel = NULL;
 static int fd;
 static int inode_track_index = 0;
 
-static void init_log() {
-  char filename[40];
-  struct tm *time_str;
+static void init_log()
+{
+	char filename[40];
+	struct tm *time_str;
 
-  time_t current_time = time(NULL);
-  time_str = localtime(&current_time);
-  strftime(filename, sizeof(filename), "/tmp/prov_%Y-%m-%d_%H:%M:%S.json", time_str);
+	time_t current_time = time(NULL);
 
-  fd = open(filename, O_RDWR | O_CREAT);
+	time_str = localtime(&current_time);
+	strftime(filename, sizeof(filename), "/tmp/prov_%Y-%m-%d_%H:%M:%S.json", time_str);
+
+	fd = open(filename, O_RDWR | O_CREAT);
 }
 
-static void sig_handler(int sig) {
-  if (sig == SIGTERM) {
-    syslog(LOG_INFO, "thoth: Received termination signal...");
-    track__destroy(skel);
-    exit(0);
-  }
+static void sig_handler(int sig)
+{
+	if (sig == SIGTERM) {
+		syslog(LOG_INFO, "thoth: Received termination signal...");
+		track__destroy(skel);
+		exit(0);
+	}
 }
 
 // Start with adding just one id into the map
-int add_inode(struct track *skel, uint32_t index, uint64_t value) {
-  int map_fd;
-  uint64_t id;
-  if (inode_track_index > INODE_MAX_ENTRY) {
-    printf("max tracking directories reached, failed to add directory\r\n");
-    return -1;
-  }
-  map_fd = bpf_object__find_map_fd_by_name(skel->obj, "inode_map");
-  id = value;
-  bpf_map_update_elem(map_fd, &inode_track_index, &id, BPF_ANY);
-  inode_track_index++;
-  return 0;
+int add_inode(struct track *skel, uint32_t index, uint64_t value)
+{
+	int map_fd;
+	uint64_t id;
+
+	if (inode_track_index > INODE_MAX_ENTRY) {
+		printf("max tracking directories reached, failed to add directory\r\n");
+		return -1;
+	}
+	map_fd = bpf_object__find_map_fd_by_name(skel->obj, "inode_map");
+	id = value;
+	bpf_map_update_elem(map_fd, &inode_track_index, &id, BPF_ANY);
+	inode_track_index++;
+	return 0;
 }
 
 // TODO
-int remove_inode(struct track *skel, uint32_t index, uint64_t value) {
-  return 0;
+int remove_inode(struct track *skel, uint32_t index, uint64_t value)
+{
+	return 0;
 }
 
-void write_to_file(struct entry_t *entry, char* buffer) 
+void write_to_file(struct entry_t *entry, char *buffer)
 {
-  // should lock here
-  spade_write_node_file(fd, entry, buffer);
-  spade_write_node_proc(fd, entry);
-  spade_write_edge(fd, entry);
-  // should unlock here
+	// should lock here
+	spade_write_node_file(fd, entry, buffer);
+	spade_write_node_proc(fd, entry);
+	spade_write_edge(fd, entry);
+	// should unlock here
 }
 
 // this is a temporary fix for resolving the file path
-void process_file_path(struct entry_t *entry, char* buffer) {
-  int path_len = 0;
-  
-  // sanity check
-  if (entry->file_path_depth <= 0 || entry->file_path_depth > PATH_DEPTH_MAX) 
-    return; 
-  
-  for (int i = entry->file_path_depth - 1; i >= 0; i--) {
-    int len = 0;
-    if (i == entry->file_path_depth - 1) { 
-      len = sprintf(buffer, "/");
-      if (len > 0)
-        path_len += len;
-    }
-    if (i == 0) {
-      len = sprintf(buffer+path_len, "%s", entry->file_path[i]);
-    } else {  
-      len = sprintf(buffer+path_len, "%s/", entry->file_path[i]);
-    }
+void process_file_path(struct entry_t *entry, char *buffer)
+{
+	int path_len = 0;
 
-    if (len > 0)
-      path_len += len;
-  }
+	// sanity check
+	if (entry->file_path_depth <= 0 || entry->file_path_depth > PATH_DEPTH_MAX)
+		return;
+
+	for (int i = entry->file_path_depth - 1; i >= 0; i--) {
+		int len = 0;
+		if (i == entry->file_path_depth - 1) {
+			len = sprintf(buffer, "/");
+			if (len > 0)
+				path_len += len;
+		}
+		if (i == 0)
+			len = sprintf(buffer + path_len, "%s", entry->file_path[i]);
+		else
+			len = sprintf(buffer + path_len, "%s/", entry->file_path[i]);
+
+		if (len > 0)
+			path_len += len;
+	}
 }
 
 int buf_process_entry(void *ctx, void *data, size_t len)
 {
-  struct entry_t *read_entry = (struct entry_t *)data; 
-  char path_buffer[TOTAL_PATH_MAX];
-  process_file_path(read_entry, (char *)&path_buffer);
-  write_to_file(read_entry, (char *)&path_buffer);
-  return 0;
+	struct entry_t *read_entry = (struct entry_t *)data;
+	char path_buffer[TOTAL_PATH_MAX];
+
+	process_file_path(read_entry, (char *)&path_buffer);
+	write_to_file(read_entry, (char *)&path_buffer);
+	return 0;
 }
 
-static int cli_process_msg(struct op_msg *msg, struct err_msg *err) {
-  DIR* dir;
+static int cli_process_msg(struct op_msg *msg, struct err_msg *err)
+{
+	DIR *dir;
 
-  if (msg->op == ADD_DIR) {
-    dir = opendir(msg->arg[0]);
-    if (dir == NULL) {
-      err->err = ERR_OK;
-      snprintf(err->msg, MSG_LEN, "error! cannot find directory: %s", msg->arg[0]);
-      return 0;
-    }
+	if (msg->op == ADD_DIR) {
+		dir = opendir(msg->arg[0]);
+		if (dir == NULL) {
+			err->err = ERR_OK;
+			snprintf(err->msg, MSG_LEN, "error! cannot find directory: %s", msg->arg[0]);
+			return 0;
+		}
 
-    struct dirent *dir_entry = readdir(dir);
-    if (dir_entry == NULL) {
-      err->err = ERR_OK;
-      snprintf(err->msg, MSG_LEN, "error adding directory to tracked directories");
-    }
-    // TODO: get response from add
-    add_inode(skel, 0, dir_entry->d_ino);
-  }
+		struct dirent *dir_entry = readdir(dir);
+		if (dir_entry == NULL) {
+			err->err = ERR_OK;
+			snprintf(err->msg, MSG_LEN, "error adding directory to tracked directories");
+		}
+		// TODO: get response from add
+		add_inode(skel, 0, dir_entry->d_ino);
+	}
 
-  if (msg->op == RM_DIR) {
-    dir = opendir(msg->arg[0]);
-    if (dir == NULL) {
-      err->err = ERR_OK;
-      snprintf(err->msg, MSG_LEN, "error! cannot find directory: %s", msg->arg[0]);
-      return 0;
-    }
+	if (msg->op == RM_DIR) {
+		dir = opendir(msg->arg[0]);
+		if (dir == NULL) {
+			err->err = ERR_OK;
+			snprintf(err->msg, MSG_LEN, "error! cannot find directory: %s", msg->arg[0]);
+			return 0;
+		}
 
-    struct dirent *dir_entry = readdir(dir);
-    if (dir_entry == NULL) {
-      err->err = ERR_OK;
-      snprintf(err->msg, MSG_LEN, "error adding directory to tracked directories");
-    }
-    // TODO: get response from remove
-    remove_inode(skel, 0, dir_entry->d_ino);
-  }
-  return 0;
+		struct dirent *dir_entry = readdir(dir);
+		if (dir_entry == NULL) {
+			err->err = ERR_OK;
+			snprintf(err->msg, MSG_LEN, "error adding directory to tracked directories");
+		}
+		// TODO: get response from remove
+		remove_inode(skel, 0, dir_entry->d_ino);
+	}
+	return 0;
 }
 
-void* cli_server(void* data) {
-  int sockfd; 
-  socklen_t client_len;
-  struct sockaddr_in server_addr, client_addr;
+void * cli_server(void *data)
+{
+	int sockfd;
+	socklen_t client_len;
+	struct sockaddr_in server_addr, client_addr;
 
-  sockfd = socket(AF_INET, SOCK_STREAM, 0);
-  if (sockfd < 0) {
-    perror("Error opening socket for cli");
-    exit(-1);
-  }
+	sockfd = socket(AF_INET, SOCK_STREAM, 0);
+	if (sockfd < 0) {
+		perror("Error opening socket for cli");
+		exit(-1);
+	}
 
-  bzero((char *) &server_addr, sizeof(server_addr));
-  server_addr.sin_family = AF_INET;
-  server_addr.sin_addr.s_addr = INADDR_ANY;
-  server_addr.sin_port = htons(CLI_PORT);
+	bzero((char *) &server_addr, sizeof(server_addr));
+	server_addr.sin_family = AF_INET;
+	server_addr.sin_addr.s_addr = INADDR_ANY;
+	server_addr.sin_port = htons(CLI_PORT);
 
-  if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
-    perror("Error binding socket for cli");
-    close(sockfd);
-    exit(-1);
-  }
+	if (bind(sockfd, (struct sockaddr *) &server_addr, sizeof(server_addr)) < 0) {
+		perror("Error binding socket for cli");
+		close(sockfd);
+		exit(-1);
+	}
 
-  listen(sockfd, 5);
-  client_len = sizeof(client_addr);
-  int newsockfd;
+	listen(sockfd, 5);
+	client_len = sizeof(client_addr);
+	int newsockfd;
 
-  while (1) {
-    newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &client_len);
+	while (1) {
+		newsockfd = accept(sockfd, (struct sockaddr *) &client_addr, &client_len);
 
-    if (newsockfd < 0) {
-      perror("error on socket accept");
-    }
+		if (newsockfd < 0)
+			perror("error on socket accept");
 
-    struct op_msg r_msg;
- 
-    int n = read(newsockfd, &r_msg, sizeof(r_msg));
-    if (n < 0) 
-      perror("error reading from socket");
-    
-    printf("%s\r\n", r_msg.arg[0]);
- 
-    // TODO: remove and replace with real error msg   
-    struct err_msg e_msg = {
-      .err = ERR_OK,
-      .msg = "Sucess!"
-    };
+		struct op_msg r_msg;
 
-    cli_process_msg(&r_msg, &e_msg);
-    
-    n = write(newsockfd, &e_msg, sizeof(e_msg));
- 
-    if (n < 0)
-      perror("error writing to socket");
+		int n = read(newsockfd, &r_msg, sizeof(r_msg));
+		if (n < 0)
+			perror("error reading from socket");
 
-    close(newsockfd);
-  }
-  return 0;
+		printf("%s\r\n", r_msg.arg[0]);
+
+		// TODO: remove and replace with real error msg
+		struct err_msg e_msg = {
+			.err = ERR_OK,
+			.msg = "Sucess!"
+		};
+
+		cli_process_msg(&r_msg, &e_msg);
+
+		n = write(newsockfd, &e_msg, sizeof(e_msg));
+
+		if (n < 0)
+			perror("error writing to socket");
+
+		close(newsockfd);
+	}
+	return 0;
 }
 
 int main(int argc, char *argv[])
 {
-  struct ring_buffer *ringbuf = NULL;
-  int err, map_fd;
-  pthread_t cli_thread_id;
+	struct ring_buffer *ringbuf = NULL;
+	int err, map_fd;
+	pthread_t cli_thread_id;
 
-  syslog(LOG_INFO, "thothd: Registering signal handler...");
-  signal(SIGTERM, sig_handler);
+	syslog(LOG_INFO, "thothd: Registering signal handler...");
+	signal(SIGTERM, sig_handler);
 
-  syslog(LOG_INFO, "thothd: Starting CLI server...");
-  int rc = pthread_create(&cli_thread_id, NULL, cli_server, NULL);
-  if (rc != 0) {
-    syslog(LOG_ERR, "thothd: error starting cli thread");
-  }
+	syslog(LOG_INFO, "thothd: Starting CLI server...");
+	int rc = pthread_create(&cli_thread_id, NULL, cli_server, NULL);
 
-  printf("starting...\r\n");
+	if (rc != 0)
+		syslog(LOG_ERR, "thothd: error starting cli thread");
 
-  skel = track__open_and_load();
-  if (!skel) {
-    syslog(LOG_ERR, "Failed to load bpf skeleton");
-    goto close_prog;
-  }
-  
-  err = track__attach(skel);
-  if (err != 0) {
-    syslog(LOG_ERR, "Error attaching skeleton\r\n");
-  }
+	printf("starting...\r\n");
 
-  init_log();
+	skel = track__open_and_load();
+	if (!skel) {
+		syslog(LOG_ERR, "Failed to load bpf skeleton");
+		goto close_prog;
+	}
 
-  if (fd < 0) {
-    syslog(LOG_ERR, "error creating log file\r\n");
-  }
-  
-  // Locate ring buffer
-  map_fd = bpf_object__find_map_fd_by_name(skel->obj, "ringbuf");
-  if (map_fd < 0) {
-    syslog(LOG_ERR, "Failed to find ring buffer map object");
-    goto close_prog;
-  }
- 
-  ringbuf = ring_buffer__new(map_fd, buf_process_entry, NULL, NULL);
-  
-  while (ring_buffer__poll(ringbuf, -1) >= 0) { 
-    // collect prov in callback
-  }
+	err = track__attach(skel);
+	if (err != 0)
+		syslog(LOG_ERR, "Error attaching skeleton\r\n");
+
+	init_log();
+
+	if (fd < 0)
+		syslog(LOG_ERR, "error creating log file\r\n");
+
+	// Locate ring buffer
+	map_fd = bpf_object__find_map_fd_by_name(skel->obj, "ringbuf");
+	if (map_fd < 0) {
+		syslog(LOG_ERR, "Failed to find ring buffer map object");
+		goto close_prog;
+	}
+
+	ringbuf = ring_buffer__new(map_fd, buf_process_entry, NULL, NULL);
+
+	while (ring_buffer__poll(ringbuf, -1) >= 0) {
+		// collect prov in callback
+	}
 close_prog:
-  close(fd);
-  track__destroy(skel);
+	close(fd);
+	track__destroy(skel);
 }
